a7f70047ee6ef74afb88f26fa0bb37a0acaea25c
Aaron Danielpour <aarondanielpour@gmail.com>
2025-11-02 17:32:32 -0500

    Added display names to the due date and IsCompleted fields.

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -12,9 +12,10 @@ namespace TimeManagementApp.Models
         public string? Title { get; set; }
         public string? Description { get; set; }
 
-        [Required]
+        [Display(Name = "Due Date")]
         [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
+        [Display(Name = "Completed?")]
         public bool IsCompleted { get; set; }
         public PriorityLevel Priority { get; set; } // 1=High, 2=Medium, 3=Low
     }
