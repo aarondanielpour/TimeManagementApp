7be0bc7fb85e1de8b4f0716aae33ea7eb87aefd5
Aaron Danielpour <aarondanielpour@gmail.com>
2025-11-01 22:10:57 -0400

    Added MVC Controller with views using EF, and a model for each task item.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TimeManagementApp.Models
+{
+    public enum PriorityLevel
+    {
+        High, Medium, Low
+    }
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public PriorityLevel Priority { get; set; } // 1=High, 2=Medium, 3=Low
+    }
+}
